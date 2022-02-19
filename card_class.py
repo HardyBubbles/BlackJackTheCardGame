f53cf487b48cd050a612391b222ec638d784287f
HardyBubbles <mukulatura@list.ru>
2022-02-19 18:07:30 +1000

    Created the new class and global variables
    Created card_class.py fill of content and global_variables.py

---
+++ card_class.py
@@ -0,0 +1,14 @@
+"""
+In this file describes the Card class
+"""
+
+
+class Card:
+
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        return self.rank + " of " + self.suit
+
---
+++ main.py
@@ -0,0 +1,6 @@
+import random
+import global_variables
+
+
+# The Game is turned on
+game_on = True
